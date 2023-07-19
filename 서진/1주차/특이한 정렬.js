da6e07e182e75421b7e84d17d204f7895a37e29d
zkdk1241 <zkdk1241@naver.com>
2023-07-19 16:17:47 +0900

    [Programmers]특이한 정렬-양서진

---
+++ 서진/1주차/특이한 정렬.js
@@ -0,0 +1,15 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/120880
+function solution(numlist, n) {
+  return numlist.sort((a, b) => {
+    // 만약 절대값이 같다면 더 큰 값을 앞에다 두기
+    if (Math.abs(a - n) === Math.abs(b - n)) {
+      return a < b ? 1 : -1
+    }
+    // 절대값 차이를 기준으로 오름차순 정렬
+    return Math.abs(a - n) - Math.abs(b - n)
+  })
+}
+
+function solution(numlist, n) {
+  return numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a)
+}
