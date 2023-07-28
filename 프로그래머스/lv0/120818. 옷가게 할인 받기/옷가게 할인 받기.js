0f238b5f50e0e707cf733ceb59b45ef98b5e5283
zkdk1241 <47853679+zkdk1241@users.noreply.github.com>
2023-07-28 15:52:10 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,11 @@
+function solution(price) {
+    let answer = 0;
+    if(price>=500000){
+        price *= 0.8; 
+    } else if(price>=300000){
+         price *= 0.9; 
+    } else if(price>=100000){
+         price *= 0.95; 
+    }
+    return Math.floor(price);
+}
